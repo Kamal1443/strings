eec39ebc400e9a343fc392d921f3d65a8169f4f8
unknown <kamolkadamov@ya.ru>
2021-11-13 19:40:56 +0500

    task-2

---
+++ task-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main (){
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("Your character is %c", n);
+
+    return 0;
+}
