6800b2ffb76b20193beab61b7a736427624d71bc
unknown <kamolkadamov@ya.ru>
2021-11-13 19:29:52 +0500

    task-1

---
+++ task-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    char ch;
+
+    printf("Enter character: ");
+    scanf("%c", &ch);
+
+    printf("\nASCII code of your character is: %d", ch);
+    return 0;
+}
